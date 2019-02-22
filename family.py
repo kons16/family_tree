3b2e3fd29c49ffb23aa4a201e4063e8967578c06
kons16 <ks1216r@gmail.com>
2019-02-22 21:07:49 +0900

    addFatherとaddMother関数の追加

--- family.py
+++ family.py
@@ -1,27 +1,52 @@
-import sys
 import tkinter as tk
 from tkinter import *
 
 
 # 名前追加ボタンを押されたときの処理
 def addNameValue(event):
-    global nameLbl
+    global label_name, setx, sety
     newName = nameBox.get()
-    nameLbl = tk.Label(canvas, text=newName)
-    nameLbl.bind("<Button-2>", showMenu)
-    nameLbl.place(x=250, y=250)
+    label_name = tk.Label(canvas, text=newName)
+    label_name.bind("<Button-1>", showMenu)
+
+    # addXX関数でセットされた位置に名前を配置
+    label_name.place(x=setx, y=sety)
+
+    # 名前入力欄をクリアする
+    nameBox.delete(0, tk.END)
 
 
 def showMenu(event):
+    global label_name, setx, sety, label_width
+    # set座標にクリックしたイベントの位置を代入
+    setx = label_name.winfo_rootx() - 5
+    sety = label_name.winfo_rooty() - 50
+    label_width = label_name.winfo_width()
     menu.post(event.x_root, event.y_root)
 
 
-def addFather(event):
+def addFather():
+    global setx, sety, label_width
+    # 家系図への線を描く
+    canvas.create_line(setx+label_width/2, sety, setx+label_width/2,
+                       sety-40, setx+label_width/2-40, sety-40)
+    # addFatherを押したラベルの座標からfatherをセットする位置を決める
+    setx = setx - 45 - label_width/2
+    sety = sety - 50
 
+
+def addMother():
+    global setx, sety, label_width
+    # 家系図への線を描く
+    canvas.create_line(setx+label_width/2, sety, setx+label_width/2,
+                       sety-40, setx+label_width/2+40, sety-40)
+    # addFatherを押したラベルの座標からfatherをセットする位置を決める
+    setx = setx + 45 + label_width/2
+    sety = sety - 50
 
 
 def deleteName():
-    nameLbl.destory()
+    label_name.destory()
 
 
 root = tk.Tk()
@@ -29,9 +54,15 @@ root.iconbitmap('test.ico')
 root.title("家系図シュミレーター")
 root.geometry("500x500")
 
+# 初期変数
+mode = 'me'
+setx = 250
+sety = 250
+label_width = 0
+
 menu = Menu(root, tearoff=False)
-menu.add_command(label="削除", command=deleteName)
 menu.add_command(label="父親を追加", command=addFather)
+menu.add_command(label="母親を追加", command=addMother)
 
 # キャンバス
 canvas = tk.Canvas(root, width=500, height=500)

8d85a04417b9f996fa14e3e364e360ec8078628e
kons16 <ks1216r@gmail.com>
2019-02-27 12:22:19 +0900

    配偶者の追加

--- family_tree.py
+++ family_tree.py
@@ -55,7 +55,7 @@ def addMother():
     canvas.create_line(setx+label_width/2, sety,
                        setx+label_width/2, sety-40,
                        setx+label_width/2+40, sety-40)
-    # addFatherを押したラベルの座標からfatherをセットする位置を決める
+
     label_width = label_name.winfo_width()
     setx = setx + 50
     sety = sety - 50
@@ -71,7 +71,7 @@ def addBig():
                        setx+label_width/2, sety-20,
                        setx+label_width/2-60, sety-20,
                        setx+label_width/2-60, sety)
-    # addFatherを押したラベルの座標からfatherをセットする位置を決める
+
     label_width = label_name.winfo_width()
     setx = setx - 60 - label_width
     sety = sety
@@ -87,9 +87,23 @@ def addLittle():
                        setx+label_width/2, sety-20,
                        setx+label_width/2+60, sety-20,
                        setx+label_width/2+60, sety)
-    # addFatherを押したラベルの座標からfatherをセットする位置を決める
+
     label_width = label_name.winfo_width()
     setx = setx + 80
+    sety = sety
+
+    # 名前を配置
+    label_name.place(x=setx, y=sety)
+
+
+def addSpouse():
+    global setx, sety, label_width, label_name
+    # 家系図への線を描く
+    canvas.create_line(setx+label_width, sety+10,
+                       setx+label_width+60, sety+10)
+
+    label_width = label_name.winfo_width()
+    setx = setx + 105
     sety = sety
 
     # 名前を配置
@@ -112,6 +126,7 @@ menu.add_command(label="父親を追加", command=addFather)
 menu.add_command(label="母親を追加", command=addMother)
 menu.add_command(label="兄姉を追加", command=addBig)
 menu.add_command(label="弟妹を追加", command=addLittle)
+menu.add_command(label="配偶者を追加", command=addSpouse)
 
 # キャンバス
 canvas = tk.Canvas(root, width=500, height=500)
@@ -129,7 +144,7 @@ addBtn.bind("<Button-1>", addNameValue)
 addBtn.pack(anchor="se", side="left")
 
 # メイン人物追加ボタン
-addBtn = tk.Button(text="メイン人物の追加", width=20)
+addBtn = tk.Button(text="メイン人物の追加", width=13)
 # <Button-1>は左クリック
 addBtn.bind("<Button-1>", mainName)
 addBtn.pack(anchor="se", side="left")
